70172739433acc8b9ae0acf1d1de5217de00da8b
Mathieu <bon.mathieu@gmail.com>
2025-01-24 23:10:20 +0100

    Improved styling of general layout. Refactored MainContent into App for better readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
 import LeftPanel from "./components/LeftPanel"
-import MainContent from "./components/MainContent"
 import letters from "./data/letters.json"
+import LetterImage from "./components/LetterImage"
+import LetterText from "./components/LetterText"
 
 const App: React.FC = () => {
 	const [selectedLetterIndex, setSelectedLetterIndex] = useState<number | null>(null)
 	const [isLeftPanelVisible, setIsLeftPanelVisible] = useState(false)
-	const lettersJSON = letters
+	// const [selectedPhotoIndex, setSelectedPhotoIndex] = useState<number | null>(null)
+	const [selectedImageType, setSelectedImageType] = useState<string>("enveloppeRecto")
+	const [isTextVisisible, setIsTextVisible] = useState<boolean>(false)
+	const selectedLetter = selectedLetterIndex !== null ? letters[selectedLetterIndex] : null
 
 	const handleShowListButtonClick = () => {
 		if (!isLeftPanelVisible) {
@@ -17,14 +21,14 @@ const App: React.FC = () => {
 	}
 
 	return (
-		<div className="w-screen h-screen flex p-4 gap-4 font-typerwriter">
+		<div className="w-screen h-screen flex p-4 font-typerwriter pl-20 py-5 px-5">
 			{/* Background image */}
 			<img
 				src="images/backgroundLight.png"
 				className="absolute inset-0 w-full h-full object-cover -z-10"
 			/>
-      {/* Folder image */}
-			<div className="">
+			{/* Menu icon */}
+			<div className="menu-icon absolute top-4 left-4">
 				<img
 					src="images/books.png"
 					className="w-full h-24 cursor-pointer hover:-rotate-12"
@@ -38,7 +42,26 @@ const App: React.FC = () => {
 					setIsLeftPanelVisible={setIsLeftPanelVisible}
 				/>
 			)}
-			<MainContent selectedLetterIndex={selectedLetterIndex} letters={lettersJSON} />
+
+			{selectedLetter && (
+				<>
+					<LetterImage
+						selectedLetter={selectedLetter}
+						selectedImageType={selectedImageType}
+						setSelectedImageType={setSelectedImageType}
+						isTextVisible={isTextVisisible}
+						setIsTextVisible={setIsTextVisible}
+					/>
+					{isTextVisisible && <LetterText selectedLetter={selectedLetter} />}
+				</>
+			)}
+
+			{selectedLetter === null && (
+				<div className="flex flex-col gap-4 pl-48 justify-center text-5xl">
+					<p>Lettres d'André à Marthe</p>
+					<p>1950 - 1953</p>
+				</div>
+			)}
 		</div>
 	)
 }
